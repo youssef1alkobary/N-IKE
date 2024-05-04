39e0e4db0c6849ad9b1e8a48d5ec6c5adfb452a5
Youssef Al-Kobary <168835912+youssef1alkobary@users.noreply.github.com>
2024-05-04 03:11:06 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+/*
+// Api
+const myJosnApiProject = `{Username: "Youssef Alkobary", Age: "16"}`;
+console.log(typeof myJosnApiProject);
+console.log(myJosnApiProject);*/
+
+// function getData() {
+//     return new Promise((res, rej) => {
+//         let users = ["Youssef", "Hayder"];
+//         if (users.length > 0) {
+//             res("Users Found");
+//         } else {
+//             rej("Users Not Found");
+//         }
+//     });
+// }
+// getData().then(
+//     (resolvedValue) => console.log(resolvedValue),
+//     (rejectedValue) => console.log(rejectedValue)
+// );
+
+
+// var btnMoon = document.querySelector('#night_moon');
+
+// function night() {
+
+//     var btnMoon = document.querySelector('#night_moon');
+
+//     btnMoon.onclick = function moon() {
+//         body.style.background = '#111';
+//     }
+
+// }
+
+
+let body = document.querySelector('#body');
+let btn = document.querySelector('#night_moon');
